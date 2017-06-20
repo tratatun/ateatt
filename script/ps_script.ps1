e7207f9217379f74379faae9811a85d63d6a1f88
Alexander Nikiforov <nikiforov.aleksandr@gmail.com>
2017-06-21 01:22:00 +0300

    37

--- script/ps_script.ps1
+++ script/ps_script.ps1
@@ -1,1 +1,6 @@
-Invoke-Command -cn localhost -ScriptBlock {Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, Publisher, InstallDate }
+$username = 'user1'
+$password = 'pAssw0rd'
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
+#$user = Get-Credential
+Invoke-Command -uri HomePC -Credential $cred -ScriptBlock {Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, Publisher, DisplayVersion, InstallDate | ?{$_.DisplayName -notlike "" } | ft} 
+#Invoke-RestMethod
