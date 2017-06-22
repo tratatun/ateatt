2a2358344fdb7366b3944cf513c30a3ab4cdeda9
Alexander Nikiforov <nikiforov.aleksandr@gmail.com>
2017-06-22 22:37:24 +0300

    36 Send-a-list-of-installed-applications

--- script/ps_script.ps1
+++ script/ps_script.ps1
@@ -1,6 +1,12 @@
-$username = 'user1'
-$password = 'pAssw0rd'
+﻿$username = 'TTN\ttn'
+$password = 'Da1mpDa2mp'
+$compName = 'ttn'
+$hostAPI = 'http://localhost:5000'
+$APIURI = '/api/values'
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
 #$user = Get-Credential
-Invoke-Command -uri HomePC -Credential $cred -ScriptBlock {Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, Publisher, DisplayVersion, InstallDate | ?{$_.DisplayName -notlike "" } | ft} 
-#Invoke-RestMethod
+$jsonData = Invoke-Command $compName -Credential $cred -ScriptBlock {Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, Publisher, DisplayVersion, InstallDate | ?{$_.DisplayName -notlike "" } | ft}| ConvertTo-Json
+
+Invoke-RestMethod -Uri ($hostAPI + $APIURI) -Body '45' -Method Post -ContentType 'application/json'
+
+#$tr = {value = $outVar| ConvertTo-Json}
