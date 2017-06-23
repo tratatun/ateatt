c7da0d18a45c368cff487c1b051be3dfb631e92c
Alexander Nikiforov <nikiforov.aleksandr@gmail.com>
2017-06-23 10:55:57 +0300

    41

---
+++ ateaTestTask/API/Models/ApplicationInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class ApplicationInfo
+    {
+        public string DisplayName { get; set; }
+        public string Publisher { get; set; }
+        public string DisplayVersion { get; set; }
+        public string InstallDate { get; set; }//":  "20170305"
+    }
+}
--- script/ps_script.ps1
+++ script/ps_script.ps1
@@ -5,8 +5,9 @@ $hostAPI = 'http://localhost:5000'
 $APIURI = '/api/values'
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
 #$user = Get-Credential
-$jsonData = Invoke-Command $compName -Credential $cred -ScriptBlock {Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, Publisher, DisplayVersion, InstallDate | ?{$_.DisplayName -notlike "" } | ft}| ConvertTo-Json
+$jsonData = Invoke-Command $compName -Credential $cred -ScriptBlock {Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, Publisher, DisplayVersion, InstallDate | ?{$_.DisplayName -notlike "" } | ConvertTo-Json}
 
-Invoke-RestMethod -Uri ($hostAPI + $APIURI) -Body '45' -Method Post -ContentType 'application/json'
+Invoke-RestMethod -Uri ($hostAPI + $APIURI) -Body $jsonData -Method Post -ContentType 'application/json'
 
 #$tr = {value = $outVar| ConvertTo-Json}
+$jsonData 
