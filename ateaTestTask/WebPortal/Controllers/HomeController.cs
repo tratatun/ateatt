aec0a6819828e27aabc119b640b49abd1786d1a3
Alexander Nikiforov <nikiforov.aleksandr@gmail.com>
2017-06-30 01:40:12 +0300

    45 init WebPortal

---
+++ ateaTestTask/WebPortal/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Uri uri = new Uri("http://atea-api.azurewebsites.net/api/values");
+            HttpClient http = new HttpClient();
+            http.DefaultRequestHeaders.Accept.Clear();
+            http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            http.DefaultRequestHeaders.Add("User-Agent", ".NETCore");
+
+            Task<string> stringTask = http.GetStringAsync("http://atea-api.azurewebsites.net/api/values?computerName=ttn");
+            string msg = stringTask.Result;
+
+            return View(msg);
+        }
+
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
