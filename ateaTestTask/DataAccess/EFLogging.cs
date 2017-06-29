e8f24fc57d30a7af6ca48a2dd71f13951e1e2f1f
Alexander Nikiforov <nikiforov.aleksandr@gmail.com>
2017-06-29 12:06:25 +0300

    43 50

--- ateaTestTask/DataAccess/EFLogging.cs
+++ ateaTestTask/DataAccess/EFLogging.cs
@@ -4,6 +4,7 @@ using System.IO;
 
 namespace EFLogging
 {
+    // Just simple logger, I got it from https://docs.microsoft.com/ru-ru/ef/core/miscellaneous/logging 
     public class MyLoggerProvider : ILoggerProvider
     {
         public ILogger CreateLogger(string categoryName)
@@ -24,7 +25,7 @@ namespace EFLogging
             public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
             {
                 //File.AppendAllText(@"C:\temp\log.txt", formatter(state, exception));
-                Console.WriteLine(formatter(state, exception));
+                Console.WriteLine(DateTime.Now.ToString("s") + " - " + formatter(state, exception));
             }
 
             public IDisposable BeginScope<TState>(TState state)
